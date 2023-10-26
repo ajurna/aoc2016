93d627b94365a37435a07b0d5d419d351ffd7d48
Ajurna <ajurna@gmail.com>
2023-10-26 15:01:06 +0100

    day 24 finished

--- day24/01.py
+++ day24/01.py
@@ -1,12 +1,6 @@
-import heapq
 from collections import defaultdict, deque
-from itertools import combinations, count
-from typing import NamedTuple, Tuple
+from typing import NamedTuple
 
-
-# spots = [0, 1, 2, 3, 4]
-# for c in combinations(spots, 2):
-#     print(c)
 
 class Point(NamedTuple):
     x: int
@@ -21,9 +15,6 @@ class Point(NamedTuple):
     def neighbors(self):
         return [self + p for p in [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)]]
 
-    def score(self, places: Tuple["Point", ...]):
-        distances = [self.distance(p) for p in places]
-        return min(distances) + 0.1 * sum(distances)  # Adjust alpha (0.1 here) as per your problem's requirement
 
 spots = {}
 grid = []
@@ -38,16 +29,10 @@ with open('01.txt') as f:
                 new_line.append(pos)
 
         grid.append(new_line)
-print(spots)
-
-for row in grid:
-    print(''.join(row))
 
 
 def search(start, places_to_visit, area):
     queue = deque()
-    # heapq.heapify(queue)
-    c = count()
     queue.append((start, places_to_visit, 0))
     visited = defaultdict(set)
     visited[places_to_visit].add(start)
@@ -70,6 +55,32 @@ def search(start, places_to_visit, area):
             queue.append((choice, new_remaining, steps))
 
 
+def search2(start, places_to_visit, area):
+    queue = deque()
+    queue.append((start, places_to_visit, 0))
+    visited = defaultdict(set)
+    visited[places_to_visit].add(start)
+    while queue:
+        position, remaining, steps = queue.popleft()
+        position: Point
+        steps += 1
+        for choice in position.neighbors():
+            if area[choice.y][choice.x] == '#':
+                continue
+            if choice in remaining:
+                new_remaining = tuple(r for r in remaining if r != choice)
+            else:
+                new_remaining = remaining
+            if choice in visited[new_remaining]:
+                continue
+            visited[new_remaining].add(choice)
+            if len(new_remaining) == 0:
+                if choice == start:
+                    return steps
+                else:
+                    new_remaining = (start,)
+            queue.append((choice, new_remaining, steps))
 
-part1 = search(spots[0], tuple(s for k, s in spots.items() if k != 0), grid)
-print(part1)
+
+print("Part 1:", search(spots[0], tuple(s for k, s in spots.items() if k != 0), grid))
+print("Part 2:", search2(spots[0], tuple(s for k, s in spots.items() if k != 0), grid))
